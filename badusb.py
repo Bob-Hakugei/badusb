cde65dda0431352ce1b8347b838791cfd3c26e00
Bob-Hakugei <bob.dev.world@gmail.com>
2023-11-11 16:58:48 +0100

    essais

---
+++ badusb.py
@@ -0,0 +1,9 @@
+from adbutils import adb
+
+# Connecter le PC et le téléphone Android via USB
+d = adb.device()
+
+# Exemple de commande pour contrôler le PC à partir du téléphone Android
+d.shell("ls")  # Exécuter la commande "ls" sur le PC
+
+# Vous pouvez ajouter d'autres commandes pour contrôler votre PC ici
